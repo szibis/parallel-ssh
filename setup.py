ca75bebf95790c709192d68934fc78821d3f9872
Panos <pkittenis@gmail.com>
2014-03-11 13:51:34 +0000

    Adding py3 setup.py support - pending py3 support in a new release of gevent

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
 setup(name='parallel-ssh',
       version='0.3',
       description='Wrapper library over paramiko to allow remote execution of tasks. Supports parallel execution on multiple hosts',
@@ -9,4 +14,16 @@ setup(name='parallel-ssh',
       url = "https://github.com/pkittenis/parallel-ssh",
       py_modules = ['pssh'],
       install_requires = ['paramiko', 'gevent'],
+      cmdclass = {'build_py': build_py},
+      classifiers = [
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: Unix',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: Windows',
+        ],
       )
